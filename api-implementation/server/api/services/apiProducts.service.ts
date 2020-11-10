5599a3fb809c9f6fd318eddea6c28da79c6b141a
195858 <swen-helge.huber@solace.com>
2020-11-10 11:13:15 +0000

    Refactored to extract persistence service

--- api-implementation/server/api/services/apiProducts.service.ts
+++ api-implementation/server/api/services/apiProducts.service.ts
@@ -1,114 +1,32 @@
 import L from '../../common/logger';
 import APIProduct = Components.Schemas.APIProduct;
-import { databaseaccess } from '../../../src/databaseaccess';
-import mongodb, { DeleteWriteOpResultObject } from 'mongodb';
-
-interface APIProductPersistent extends APIProduct {
-  _id: string
-}
+import {PersistenceService} from './persistence.service';
 
 export class ApiProductsService {
-  static getCollection = () => {
-    return databaseaccess.client.db('solace-platform').collection('api_products');
+
+  private persistenceService: PersistenceService;
+  constructor(){
+    this.persistenceService = new PersistenceService('api_products');
   }
 
   all(): Promise<APIProduct[]> {
-    return new Promise<APIProduct[]>((resolve, reject) => {
-      const collection: mongodb.Collection = ApiProductsService.getCollection();
-      collection.find({}).toArray(
-        (err, items) => {
-          if (err) {
-            L.error('Caught error', err);
-
-            reject(err);
-          } else {
-            items.forEach((item) => {
-              delete item._id;
-            });
-            resolve(items);
-          }
-        }
-      );
-    });
+    return this.persistenceService.all();
   }
 
   byName(name: string): Promise<APIProduct> {
-    return new Promise<APIProduct>((resolve, reject) => {
-      const collection: mongodb.Collection = ApiProductsService.getCollection();
-      collection.findOne({ _id: name }).then(
-        (item) => {
-          if (!item) {
-            reject
-          }
-          delete item._id;
-          resolve(item);
-        }
-
-      ).catch((e) => {
-        reject(e);
-      });
-    });
+    return this.persistenceService.byName(name);
   }
 
   delete(name: string): Promise<number> {
-    return new Promise<number>((resolve, reject) => {
-      const collection: mongodb.Collection = ApiProductsService.getCollection();
-      collection.deleteOne({ _id: name }).then(
-        (item: DeleteWriteOpResultObject) => {
-          L.info(` deleted count: ${item.deletedCount}`);
-          if (item.deletedCount==1) {
-            resolve(204)
-          } else {
-            reject(404);
-          }
-        }
-
-      ).catch((e) => {
-        reject(e);
-      });
-    });
+    return this.persistenceService.delete(name);
   }
  
   create(body: APIProduct): Promise<APIProduct> {
-    return new Promise<APIProduct>((resolve, reject) => {
-      L.info(`adding API Product`);
-      const collection: mongodb.Collection = ApiProductsService.getCollection();
-      var persistent: APIProductPersistent = {
-        _id: body.name,
-        apis: body.apis,
-        displayName: body.displayName,
-        attributes: body.attributes,
-        name: body.name,
-        pubResources: body.pubResources,
-        subResources: body.subResources,
-        approvalType: body.approvalType,
-        description: body.description,
-        environments: body.environments,
-        scopes: body.scopes
-      };
-      collection.insertOne(persistent).then((v: mongodb.InsertOneWriteOpResult<any>) => {
-        resolve(body);
-      }).catch((e) => {
-        L.error(e);
-        reject(422);
-      });
-    });
+    return this.persistenceService.create(body.name, body);
   }
 
   update(name: string, body: APIProduct): Promise<APIProduct> {
-    return new Promise<APIProduct>((resolve, reject) => {
-      L.info(`adding API Product`);
-      const collection: mongodb.Collection = ApiProductsService.getCollection();
-      collection.updateOne({ _id: name }, {$set: body}).then((v: mongodb.UpdateWriteOpResult) => {
-        this.byName(name).then((p) => {
-          resolve(p);
-        });
-
-      }).catch((e) => {
-        L.error(e);
-        reject(422);
-      });
-    });
+    return this.persistenceService.update(name, body);
   }
 }
 
---
+++ api-implementation/server/api/services/persistence.service.ts
@@ -0,0 +1,130 @@
+import L from '../../common/logger';
+import { databaseaccess } from '../../../src/databaseaccess';
+import mongodb, { DeleteWriteOpResultObject } from 'mongodb';
+
+export class PersistenceService {
+  private collection : string;
+  getCollection = () => {
+    return databaseaccess.client.db('solace-platform').collection(this.collection);
+  }
+  constructor(collection: string){
+    this.collection = collection;
+  }
+
+  all(query?: object): Promise<any[]> {
+    if (!query){
+      query = {};
+    }
+    return new Promise<any[]>((resolve, reject) => {
+      const collection: mongodb.Collection = this.getCollection();
+      collection.find(query).toArray(
+        (err, items) => {
+          if (err) {
+            L.error('Caught error', err);
+
+            reject(err);
+          } else {
+            items.forEach((item) => {
+              delete item._id;
+            });
+            resolve(items);
+          }
+        }
+      );
+    });
+  }
+
+  byName(name: string, query?: any): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      const collection: mongodb.Collection = this.getCollection();
+      var q = query;
+      if (q){
+        q._id = name;
+      } else  {
+        q = { _id: name };
+      }
+      L.info(q);
+      collection.findOne(q).then(
+        (item) => {
+          if (!item) {
+            resolve(null);
+          }
+          delete item._id;
+          resolve(item);
+        }
+
+      ).catch((e) => {
+        reject(e);
+      });
+    });
+  }
+
+  delete(name: string, query?: any): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      const collection: mongodb.Collection = this.getCollection();
+      var q = query;
+      if (q){
+        q._id = name;
+      } else  {
+        q = { _id: name };
+      }
+      L.info(q);
+      collection.deleteOne(q).then(
+        (item: DeleteWriteOpResultObject) => {
+          L.info(` deleted count: ${item.deletedCount}`);
+          if (item.deletedCount == 1) {
+            resolve(204)
+          } else {
+            reject(404);
+          }
+        }
+
+      ).catch((e) => {
+        reject(e);
+      });
+    });
+  }
+
+  create(_id: string, body: any): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      L.info(`adding ${this.collection} with _id ${_id}`);
+      const collection: mongodb.Collection = this.getCollection();
+      body._id = _id;
+      collection.insertOne(body).then((v: mongodb.InsertOneWriteOpResult<any>) => {
+        resolve(body);
+      }).catch((e) => {
+        L.error(e);
+        reject(422);
+      });
+    });
+  }
+
+  update(_id: string, body: any, query?: any): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      L.info(`patching ${this.collection} with _id ${_id}`);
+      const collection: mongodb.Collection = this.getCollection();
+      var q = query;
+      if (q){
+        q._id = name;
+      } else  {
+        q = { _id: name };
+      }
+      collection.updateOne(q, { $set: body }).then((v: mongodb.UpdateWriteOpResult) => {
+        L.info(v);
+        if (v.matchedCount == 0) {
+          reject(404);
+        }
+        this.byName(_id).then((p) => {
+          resolve(p);
+        }).catch((e) => {
+          reject(e);
+        });
+
+      }).catch((e) => {
+        reject(422);
+      });
+    });
+  }
+}
+
+//export default new PersistenceService();
