ce0e3bd34627e5824a65e020db646d0b3edcd3da
195858 <3858485+195858@users.noreply.github.com>
2021-02-02 16:35:09 +0000

    Fix APIs response for all operations

--- api-implementation/server/api/controllers/apis/controller.ts
+++ api-implementation/server/api/controllers/apis/controller.ts
@@ -3,55 +3,38 @@ import { NextFunction, Request, Response } from 'express';
 import { ErrorResponseInternal } from '../../middlewares/error.handler';
 import Format = Paths.$OrganizationApis$ApiName.Get.Parameters.Format;
 import AsyncAPIHelper from '../../../../src/asyncapihelper'
+import { resolve } from 'path';
 export class Controller {
-  all(req: Request, res: Response): void {
-    ApisService.all().then((r) => res.json(r));
+  all(req: Request, res: Response, next: NextFunction): void {
+    ApisService.all().then((r) => res.json(r)).catch(e => {
+      L.error(e);
+      next(e);
+    });
   }
 
-  byName(req: Request, res: Response, next: NextFunction): void {
-    ApisService.byName(req.params['name']).then((r) => {
-      if (r) {
-        var contentType: Format = req.query['format'] as Format;
-        L.info(contentType);
-        if (contentType == "application/json") {
-          if (AsyncAPIHelper.getContentType(r) == "application/json") {
-            res.status(200).contentType(contentType).send(r);
-          } else {
-            res.status(200).contentType(contentType).send(AsyncAPIHelper.YAMLtoJSON(r));
-          }
-        } else if (contentType == "application/x-yaml") {
-          if (AsyncAPIHelper.getContentType(r) == "application/x-yaml") {
-            res.status(200).contentType(contentType).send(r);
-          } else {
-            res.status(200).contentType(contentType).send(AsyncAPIHelper.JSONtoYAML(r));
-          }
-        } else {
-          res.status(200).contentType(AsyncAPIHelper.getContentType(r)).send(r);
-        }
-      } else {
-        next(new ErrorResponseInternal(404, `Not found`));
-      }
-    }).catch((e) => next(e));
-  }
+  async byName(req: Request, res: Response, next: NextFunction): Promise<void> {
+    return new Promise<any>(async (reject, resolve) => {
+      var r: string = await ApisService.byName(req.params['name']);
+      Controller.handleResponse(r, req, res, next);
+      resolve();
+    }).catch((e) => {
+      L.error(e);
+      next(e);
+      resolve();
+    });
+  };
+
 
   create(req: Request, res: Response, next: NextFunction): void {
     ApisService.create(req.params['name'], req.body).then((r) => {
-      if (r) {
-        res.status(201).json(r).send();
-      }
-      else
-        next(new ErrorResponseInternal(500, `No response`));
+      Controller.handleResponse(r, req, res, next);
     }).catch((e) => next(e));
   }
 
 
   update(req: Request, res: Response, next: NextFunction): void {
     ApisService.update(req.params['name'], req.body).then((r) => {
-      if (r) {
-        res.status(200).json(r).send();
-      }
-      else
-        next(new ErrorResponseInternal(500, `No response`));
+      Controller.handleResponse(r, req, res, next);
     }).catch((e) => next(e));
   }
 
@@ -59,6 +42,30 @@ export class Controller {
     ApisService.delete(req.params['name']).then((r) => {
       res.status(r).send();
     }).catch((e) => next(e));
+  }
+
+  static handleResponse(r, req, res, next) {
+    if (r) {
+      var contentType: Format = req.query['format'] as Format;
+      L.info(contentType);
+      if (contentType == "application/json") {
+        if (AsyncAPIHelper.getContentType(r) == "application/json") {
+          res.status(200).contentType(contentType).send(r);
+        } else {
+          res.status(200).contentType(contentType).send(AsyncAPIHelper.YAMLtoJSON(r));
+        }
+      } else if (contentType == "application/x-yaml") {
+        if (AsyncAPIHelper.getContentType(r) == "application/x-yaml") {
+          res.status(200).contentType(contentType).send(r);
+        } else {
+          res.status(200).contentType(contentType).send(AsyncAPIHelper.JSONtoYAML(r));
+        }
+      } else {
+        res.status(200).contentType(AsyncAPIHelper.getContentType(r)).send(r);
+      }
+    } else {
+      next(new ErrorResponseInternal(404, `Not found`));
+    }
   }
 
 
