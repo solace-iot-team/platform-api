b6eba8153f95c560a4f47543ce24b9338766056d
195858 <3858485+195858@users.noreply.github.com>
2021-02-12 14:54:17 +0000

    Add servers object to App's Async API

--- api-implementation/server/api/services/apps.service.ts
+++ api-implementation/server/api/services/apps.service.ts
@@ -11,6 +11,8 @@ import AppResponse = Components.Schemas.AppResponse;
 import AppPatch = Components.Schemas.AppPatch;
 import ApiProduct = Components.Schemas.APIProduct;
 
+import AsyncAPIHelper from '../../../src/asyncapihelper'
+import { AsyncAPIServer } from '../../../src/model/asyncapiserver';
 
 export interface APISpecification {
   name: string,
@@ -45,25 +47,50 @@ export class AppsService {
     return appList;
   }
 
-   async apiList(appName: string): Promise<string[]> {
+  async apiList(appName: string): Promise<string[]> {
     var app = await this.persistenceService.byName(appName);
     var apiList: string[] = [];
-    for (var productName of app.apiProducts){
+    for (var productName of app.apiProducts) {
       var apiProduct: ApiProduct = await ApiProductsService.byName(productName);
       apiList = apiList.concat(apiProduct.apis);
     }
     return apiList;
   }
- 
+
   async byName(name: string): Promise<AppResponse> {
     var app: App = await this.persistenceService.byName(name);
-    
+
     return DevelopersService.appByName(app["ownerId"], app.name);
   }
   async apiByName(appName: string, name: string): Promise<string> {
     var app = await this.persistenceService.byName(appName);
     var envs = await BrokerService.getMessagingProtocols(app);
-    return ApisService.byName(name);
+    var servers = {};
+    for (var env of envs) {
+      for (var protocol of env.messagingProtocols) {
+        var server: AsyncAPIServer = {
+          protocol: protocol.protocol.name,
+          protocolVersion: protocol.protocol.version,
+          url: protocol.uri
+        }
+        var serverKey: string = env.name;
+        if (env.messagingProtocols.length > 1) {
+          serverKey = `${env.name}-${protocol.protocol.name}`;
+        }
+        servers[serverKey] = server;
+      }
+    }
+    var spec = await ApisService.byName(name);
+    // parse the spec - try to treat as JSON, if ails treat as YAML
+    var specModel = null;
+    try {
+      specModel = JSON.parse(spec);
+    }
+    catch (e) {
+      specModel = JSON.parse(AsyncAPIHelper.YAMLtoJSON(spec));
+    }
+    specModel.servers = servers;
+    return JSON.stringify(specModel);
   }
 }
 
---
+++ api-implementation/src/model/asyncapiserver.ts
@@ -0,0 +1,6 @@
+export interface AsyncAPIServer {
+	url: string,
+	protocol: string,
+	protocolVersion: string
+	description?: string,
+}
