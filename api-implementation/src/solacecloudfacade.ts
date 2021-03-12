1d116ac65261541f02367b949b0d5860006c6889
195858 <3858485+195858@users.noreply.github.com>
2021-03-12 13:16:56 +0000

    fix: ensure all cloud API promise rejections and errors are caught

--- api-implementation/src/solacecloudfacade.ts
+++ api-implementation/src/solacecloudfacade.ts
@@ -1,50 +1,46 @@
 import L from '../server/common/logger';
-import C from 'cls-hooked';
 
 import Environment = Components.Schemas.Environment;
 import { OpenAPI, Service, ServicesService, ServiceResponse, ServicesResponse, ApiError } from "./clients/solacecloud";
 
 import getToken from './cloudtokenhelper';
+import { ErrorResponseInternal } from '../server/api/middlewares/error.handler';
 
 class SolaceCloudFacade {
 	constructor() {
 		OpenAPI.TOKEN = getToken;
 	}
 
-	public getServiceByEnvironment(e: Environment): Promise<Service> {
-		return new Promise<Service>((resolve, reject) => {
-			var result: Promise<any> = ServicesService.getService(e.serviceId);
-
-			result.then((value: ServiceResponse) => {
-				if (!value) {
-					reject(404);
-				} else if (value.data == null) {
-					reject(400);
-				} else {
-					resolve(value.data);
-				}
-			});
-			result.catch((val) => {
-				L.error(val);
-
-				reject("API Error" + val)
-			});
-
-		});
+	public async getServiceByEnvironment(e: Environment): Promise<Service> {
+		try {
+			var result: ServiceResponse = await ServicesService.getService(e.serviceId);
+			if (result == null) {
+				throw new ErrorResponseInternal(404, `Service ${e.serviceId} does not exist`);
+			} else {
+				return result.data;
+			}
+		}
+		catch (e) {
+			L.error(e);
+			throw new ErrorResponseInternal(500, `API Error ${e}`);
+		}
 	}
-	public getServices(): Promise<Service[]> {
-		return new Promise<Service[]>((resolve, reject) => {
-			ServicesService.listServices().then((value: ServicesResponse) => {
-				if (!value)
-					reject(404);
-				resolve(value.data);
-			}).catch((val: ApiError) => {
-				L.error(val);
-				reject(val)
-			});
 
-		});
+	public async getServices(): Promise<Service[]> {
+				try {
+			var result: ServicesResponse = await ServicesService.listServices();
+			if (result == null) {
+				throw new ErrorResponseInternal(404, `No services found`);
+			} else {
+				return result.data;
+			}
+		}
+		catch (e) {
+			L.error(e);
+			throw new ErrorResponseInternal(500, `API Error ${e}`);
+		}
 	}
+
 	public async validate(token: string): Promise<boolean> {
 
 		OpenAPI.TOKEN = token;
