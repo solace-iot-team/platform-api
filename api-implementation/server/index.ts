aa0ced96ded5162ec16fe17484dc0df4f9edf96e
195858 <3858485+195858@users.noreply.github.com>
2021-03-15 10:15:35 +0000

    feat: added unhandled promise catch all for debugging run

--- api-implementation/server/index.ts
+++ api-implementation/server/index.ts
@@ -2,12 +2,12 @@ import './common/env';
 import Server from './common/server';
 import L from './common/logger';
 import routes from './routes';
-import {databaseaccess} from '../src/databaseaccess';
+import { databaseaccess } from '../src/databaseaccess';
 
 type serverCallback = () => void;
 
 const callback: serverCallback = async () => {
-   L.info(`Listening on port ${port}`);
+  L.info(`Listening on port ${port}`);
   try {
     await databaseaccess.connect(process.env.DB_URL || `mongodb://@localhost:27017/solace-platform?retryWrites=true&w=majority`);
     L.info(`Connected to Mongo!`);
@@ -16,6 +16,14 @@ const callback: serverCallback = async () => {
   }
 
 };
+
+
+if (L.isLevelEnabled('debug')) {
+  L.info('Activating unhandled promise logger');
+  process.on('unhandledRejection', error => {
+    L.debug(`unhandled rejection ${JSON.stringify(error)}`);
+  });
+}
 
 const port = parseInt(process.env.PLATFORM_PORT) || 3000;
 var server = new Server().router(routes).listenWithCallback(port, callback);
