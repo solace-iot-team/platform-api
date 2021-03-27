033197c567d227aa271fbef3c901fe85af38c78a
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-03-27 11:45:51 +0000

    release test

---
+++ release/test/server/stop.server.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+scriptDir=$(cd $(dirname "$0") && pwd);
+scriptName=$(basename $(test -L "$0" && readlink "$0" || echo "$0"));
+
+############################################################################################################################
+# Run
+
+echo " >>> Stop server ..."
+  ps -eo pid,args | grep "server/index.ts" | while read pid args; do
+    echo "pid=$pid"
+    kill -SIGKILL $pid > /dev/null 2>&1 || true
+  done
+echo " >>> Success."
+exit 0
+
+###
+# The End.
