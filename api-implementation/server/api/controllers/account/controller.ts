b5deff9dce9df0c25a32a1c9e75e2ddbe43377d9
195858 <3858485+195858@users.noreply.github.com>
2021-03-29 13:30:38 +0100

    fix: do not attempt to set headers after json()

--- api-implementation/server/api/controllers/account/controller.ts
+++ api-implementation/server/api/controllers/account/controller.ts
@@ -4,7 +4,7 @@ import SolaceCloudFacade from '../../../../src/solacecloudfacade';
 export class Controller {
   services(req: Request, res: Response, next: NextFunction): void {
     SolaceCloudFacade.getServices()
-      .then((r) => res.json(r).status(200).send())
+      .then((r) => res.json(r))
       .catch((e) => next(e));
   }
 }
