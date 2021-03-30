5e1c0cfce83bb657d154a14d9909b2e2dfb1e9e8
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-03-30 14:27:45 +0100

    test workflow

--- release/platform-api-server/docker/release.sh
+++ release/platform-api-server/docker/release.sh
@@ -23,22 +23,15 @@ echo " >>> Build Docker Image ..."
   if [[ $code != 0 ]]; then echo ">>> ERROR - code=$code - $runScript' - $scriptName"; exit 1; fi
 echo " >>> Success."
 
-echo " >>> Publish Docker Image ..."
-  echo ">>> ERROR - publish the image to docker hub now "; exit 1;
-  runScript="xxx"
-  $runScript
-  code=$?;
-  if [[ $code != 0 ]]; then echo ">>> ERROR - code=$code - $runScript' - $scriptName"; exit 1; fi
-echo " >>> Success."
-
-
-# echo " >>> Starting release of platform-api-openapi ..."
-#   cd $scriptDir
-#   runScript="npm publish"
-#   # runScript="npm publish --dry-run"
+# echo " >>> Publish Docker Image ..."
+#   echo ">>> ERROR - publish the image to docker hub now ";
+#   exit 1;
+#   runScript="xxx"
 #   $runScript
-#   code=$?; if [[ $code != 0 ]]; then echo ">>> ERROR - code=$code - $runScript' - $scriptName"; exit 1; fi
+#   code=$?;
+#   if [[ $code != 0 ]]; then echo ">>> ERROR - code=$code - $runScript' - $scriptName"; exit 1; fi
 # echo " >>> Success."
+
 
 
 ###
