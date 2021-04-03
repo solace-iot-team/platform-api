b09edab61aaef4179915e7e8c5ed1f0579d49083
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2021-04-03 11:17:53 +0100

    added timeout and checks if server is initialized

--- release/test/server/docker/start.server.sh
+++ release/test/server/docker/start.server.sh
@@ -49,10 +49,36 @@ echo " >>> Starting server in docker..."
   export LOG_LEVEL=debug
   export APP_ID=platform-api-server
   export FILE_USER_REGISTRY=$fileUserRegistry
+  export DOCKER_CLIENT_TIMEOUT=120
+  export COMPOSE_HTTP_TIMEOUT=120
 
   docker-compose -f "$dockerComposeFile" up -d
   if [[ $? != 0 ]]; then echo " >>> ERROR: docker compose with '$dockerComposeFile'"; exit 1; fi
   docker ps -a
+
+  echo "   >>> check docker logs if server up ..."
+    # wait until initialized
+    WORKING_DIR=$scriptDir/tmp; mkdir -p $WORKING_DIR; rm -rf $WORKING_DIR/*;
+    dockerLogsFile="$WORKING_DIR/docker.logs"
+    isInitialized=0; checks=0
+    until [[ $isInitialized -gt 2 || $checks -gt 10 ]]; do
+      ((checks++))
+      echo "   check: $checks"
+      docker logs $CONTAINER_NAME > $dockerLogsFile
+      entryListeningOnPort=$(grep -n -e "Listening on port $PLATFORM_PORT" $dockerLogsFile)
+      echo "      - entryListeningOnPort='$entryListeningOnPort'"
+      if [ ! -z "$entryListeningOnPort" ]; then ((isInitialized++)); fi
+      entryConnected2Mongo=$(grep -n -e "Connected to Mongo" $dockerLogsFile)
+      echo "      - entryConnected2Mongo='$entryConnected2Mongo'"
+      if [ ! -z "$entryConnected2Mongo" ]; then ((isInitialized++)); fi
+      entryLoadedUserRegistry=$(grep -n -e "Loaded user registry" $dockerLogsFile)
+      echo "      - entryLoadedUserRegistry='$entryLoadedUserRegistry'"
+      if [ ! -z "$entryLoadedUserRegistry" ]; then ((isInitialized++)); fi
+      if [ $isInitialized -lt 3 ]; then sleep 2s; fi
+    done
+    if [ $isInitialized -lt 3 ]; then echo " >>> ERROR: server is not initialized, checks=$checks"; exit 1; fi
+  echo "   >>> success."
+  
 echo " >>> Success."
 
   # docker exec -ti platform-api-server bash
