50360636a19e04e61115ff6ccfacd54c6ec1d54a
195858 <3858485+195858@users.noreply.github.com>
2022-02-02 15:50:44 +0000

    feat-enhanced-logging
    Logger includes calling function and current request id (for HTTP request processing) if NODE_ENV environment variable is set to "development"

--- api-implementation/server/common/logger.ts
+++ api-implementation/server/common/logger.ts
@@ -2,21 +2,19 @@ import pino from 'pino';
 import pinoCaller from 'pino-caller';
 import { ns } from '../api/middlewares/context.handler';
 const logFile: string = process.env.LOG_FILE;
-import { ContextConstants } from './constants';
-const getRequestId = function(){
- if(ns != null && ns.getStore()&& ns.getStore().get(ContextConstants.REQUEST_ID)){
-  return ns.getStore().get(ContextConstants.REQUEST_ID);
- } else {
-   return 'n/a';
- }
-}
 
-let logger = pino({
+
+export let logger = pino({
   name: process.env.APP_ID,
   level: process.env.LOG_LEVEL,
 }, logFile ? pino.destination(logFile) : pino.destination(1));
 logger = process.env.NODE_ENV === 'development'?pinoCaller(logger):logger;
-logger = logger.child({
-  requestId: getRequestId,
+
+const proxyLogger = new Proxy(logger, {
+  get(target, property, receiver) {
+    target = ns.getStore()?.get('logger') || target;
+    return Reflect.get(target, property, receiver);
+  },
 });
-export default logger;
+export default process.env.NODE_ENV === 'development'?proxyLogger:logger;
+
