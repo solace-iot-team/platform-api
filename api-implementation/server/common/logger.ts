69fdcf459bcc6026bd9c4d99ed3432846aeaf21e
195858 <3858485+195858@users.noreply.github.com>
2022-02-02 14:26:50 +0000

    feat-enhanced-logging-add-calling-function

--- api-implementation/server/common/logger.ts
+++ api-implementation/server/common/logger.ts
@@ -1,10 +1,22 @@
 import pino from 'pino';
-
+import pinoCaller from 'pino-caller';
+import { ns } from '../api/middlewares/context.handler';
 const logFile: string = process.env.LOG_FILE;
+import { ContextConstants } from './constants';
+const getRequestId = function(){
+ if(ns != null && ns.getStore()&& ns.getStore().get(ContextConstants.REQUEST_ID)){
+  return ns.getStore().get(ContextConstants.REQUEST_ID);
+ } else {
+   return 'n/a';
+ }
+}
 
-const logger = pino({
+let logger = pino({
   name: process.env.APP_ID,
   level: process.env.LOG_LEVEL,
 }, logFile ? pino.destination(logFile) : pino.destination(1));
-
+logger = process.env.NODE_ENV === 'development'?pinoCaller(logger):logger;
+logger = logger.child({
+  requestId: getRequestId,
+});
 export default logger;
