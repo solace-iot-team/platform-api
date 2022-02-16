2a3e8ecbbb16dc5d5f15f8c0c19409aa131462d3
195858 <3858485+195858@users.noreply.github.com>
2022-02-16 10:48:07 +0000

    fix-org-service-guard-condition-token-validation

--- api-implementation/server/api/services/organizations.service.ts
+++ api-implementation/server/api/services/organizations.service.ts
@@ -125,6 +125,7 @@ export class OrganizationsService {
         && (!body[ContextConstants.CLOUD_TOKEN].cloud.token || !body[ContextConstants.CLOUD_TOKEN].eventPortal.token)) {
         const orgInDb: Organization = await this.persistenceService.byName(name);
         if (!body[ContextConstants.CLOUD_TOKEN].cloud.token
+          && orgInDb[ContextConstants.CLOUD_TOKEN]
           && orgInDb[ContextConstants.CLOUD_TOKEN].cloud
           && orgInDb[ContextConstants.CLOUD_TOKEN].cloud.token) {
           body[ContextConstants.CLOUD_TOKEN].cloud.token = orgInDb[ContextConstants.CLOUD_TOKEN].cloud.token;
