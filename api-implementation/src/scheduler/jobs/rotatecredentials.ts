2ec06c3f596be81ddc8e0ea645a8113e63bc0e55
195858 <3858485+195858@users.noreply.github.com>
2022-05-24 15:35:16 +0100

    feat-app-credentials-rotation-refactor-to-agenda
    migrate from bree to agenda

--- api-implementation/src/scheduler/jobs/rotatecredentials.ts
+++ api-implementation/src/scheduler/jobs/rotatecredentials.ts
@@ -1,6 +1,6 @@
-import { parentPort, workerData } from 'node:worker_threads';
+//import { parentPort, workerData } from 'node:worker_threads';
 import L from '../../../server/common/logger';
-import { databaseaccess } from '../../databaseaccess';
+//import { databaseaccess } from '../../databaseaccess';
 import AppHelper from '../../apphelper';
 import AppsService from '../../../server/api/services/apps.service';
 import TeamsService from '../../../server/api/services/teams.service';
@@ -11,56 +11,75 @@ import AppPatch = Components.Schemas.AppPatch;
 //export const ns = new AsyncLocalStorage();
 import ContextRunner from '../contextrunner';
 import Organization = Components.Schemas.Organization;
-(async () => {
-  L.info(`Init rotate  credentials ${workerData.job.worker.workerData}`);
-  const dbURL = process.env.DB_URL || `mongodb://@localhost:27017/solace-platform?retryWrites=true&w=majority`;
-  let isConnected = false;
-  while (!isConnected) {
-    try {
-      await databaseaccess.connect(dbURL);
-      L.info(`Connected to Mongo!`);
-      isConnected = true;
-    } catch (err) {
-      L.error(err, `Unable to connect to Mongo, err=${JSON.stringify(err)}. Continue retrying`);
 
-    }
+import { AgendaJobSpec, AgendaJobData } from '../taskscheduler';
+import { Job } from 'agenda';
+
+export class AppRotateCredentialsJobSpec implements AgendaJobSpec {
+  jobName: string;
+  orgName: string;
+  data: AgendaJobData;
+
+  constructor() {
+    this.jobName = 'rotateAppCredentials';
   }
-})();
-
-
-async function doTheJob() {
-  const now: number = Date.now();
-  const apps: App[] = await AppsService.all({ "credentials.expiresAt": { $gt: -1, $lt: now } });
-  L.info(`results ${apps.length}`);
-  for (const app of apps) {
-    if (app.credentials && app.credentials.secret) {
-      L.debug(`now: ${now} expiresAt: ${app.credentials.expiresAt}`);
-      app.credentials.secret.consumerSecret = AppHelper.generateConsumerSecret();
-      AppHelper.resetCredentialsDates(app);
-      const appUpdate: AppPatch = {
-        credentials: app.credentials,
-      };
-      switch (app['appType']) {
-        case 'team': {
-          await TeamsService.updateApp(app['ownerId'], app.name, appUpdate);
-          break;
-        }
-        case 'developer': {
-          await DevelopersService.updateApp(app['ownerId'], app.name, appUpdate);
-          break;
-        }
-        default: {
-          break;
-        }
-      }
-    }
-  }
-  parentPort?.postMessage('done');
 
 }
 
-(async () => {
-  const org: Organization = workerData.job.worker.workerData as Organization;
-  await ContextRunner(org, doTheJob);
+// (async () => {
+//   L.info(`Init rotate  credentials ${workerData.job.worker.workerData}`);
+//   const dbURL = process.env.DB_URL || `mongodb://@localhost:27017/solace-platform?retryWrites=true&w=majority`;
+//   let isConnected = false;
+//   while (!isConnected) {
+//     try {
+//       await databaseaccess.connect(dbURL);
+//       L.info(`Connected to Mongo!`);
+//       isConnected = true;
+//     } catch (err) {
+//       L.error(err, `Unable to connect to Mongo, err=${JSON.stringify(err)}. Continue retrying`);
 
-}) ();
+//     }
+//   }
+// })();
+
+export class OrganizationAppsRotateCredentials {
+  static async doRotateCredentials() {
+    const now: number = Date.now();
+    const apps: App[] = await AppsService.all({ "credentials.expiresAt": { $gt: -1, $lt: now } });
+    L.info(`results ${apps.length}`);
+    for (const app of apps) {
+      if (app.credentials && app.credentials.secret) {
+        L.debug(`now: ${now} expiresAt: ${app.credentials.expiresAt}`);
+        app.credentials.secret.consumerSecret = AppHelper.generateConsumerSecret();
+        AppHelper.resetCredentialsDates(app);
+        const appUpdate: AppPatch = {
+          credentials: app.credentials,
+        };
+        switch (app['appType']) {
+          case 'team': {
+            await TeamsService.updateApp(app['ownerId'], app.name, appUpdate);
+            break;
+          }
+          case 'developer': {
+            await DevelopersService.updateApp(app['ownerId'], app.name, appUpdate);
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }
+    }
+    //parentPort?.postMessage('done');
+
+  }
+
+  static async rotateCredentials(job: Job) {
+    const data: AgendaJobData = job.attrs.data as AgendaJobData;
+
+    L.error(`rotating credentials in ${data.orgName}`);
+    const org: Organization = data.org;
+    await ContextRunner(org, OrganizationAppsRotateCredentials.doRotateCredentials);
+
+  }
+}
