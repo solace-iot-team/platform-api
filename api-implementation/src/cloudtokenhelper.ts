07ec8830a183c15412e1b1d23387d03f0c955e65
195858 <3858485+195858@users.noreply.github.com>
2022-09-26 13:44:28 +0100

    feat-org-setting-ep2.0-serviceregistry
    Added to organization entity-  type of PS+ service registry that is used to look up broker configuration endpoints and capabilities. Defaults to "platform" if omitted which means PS+ services are looked up directly via the Solace Cloud Platform API. "eventportal" uses the messaging services configured in Event Portal 2.0 which allows addition of self-managed brokers.

--- api-implementation/src/cloudtokenhelper.ts
+++ api-implementation/src/cloudtokenhelper.ts
@@ -4,6 +4,7 @@ import { ns } from '../server/api/middlewares/context.handler';
 import fetch from 'fetch-with-proxy';
 import { ContextConstants } from '../server/common/constants';
 import { isString } from './typehelpers';
+import Organization = Components.Schemas.Organization;
 
 export async function getCloudToken(): Promise<string> {
   let token: any = null;
@@ -95,6 +96,15 @@ export async function resolve(resolver: any) {
 export function getOrg(): string {
   let orgName: any = null;
   orgName = ns.getStore().get(ContextConstants.ORG_NAME);
+  if (orgName == null) {
+    throw new ErrorResponseInternal(500, `Org is missing from context!`);
+  }
+  return orgName;
+}
+
+export function getOrgObject(): Organization {
+  let orgName: any = null;
+  orgName = ns.getStore().get(ContextConstants.ORG_OBJECT);
   if (orgName == null) {
     throw new ErrorResponseInternal(500, `Org is missing from context!`);
   }
--- api-implementation/src/serviceregistryfactory.ts
+++ api-implementation/src/serviceregistryfactory.ts
@@ -1,11 +1,13 @@
+import { getOrg, getOrgObject } from "./cloudtokenhelper";
 import EPServiceRegistry2 from "./ep.serviceregistry.2";
 import { ServiceRegistry } from "./serviceregistry";
 import solacecloudfacade from "./solacecloudfacade";
 
 class ServiceRegistryFactory{
     public getRegistry(): ServiceRegistry {
-        const epVersion = process.env.SERVICE_REGISTRY || 'platform';
-        if (epVersion == 'eventportal'){
+        const org = getOrgObject();
+        const registryType = org.serviceRegistry?org.serviceRegistry:'platform';
+        if (registryType == 'eventportal'){
             return EPServiceRegistry2;
         } else {
             return solacecloudfacade;
